59dfbfbd0f0803475b53d6c4beae0fc2bb2994be
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2025-06-12 11:04:04 +0300

    feat: navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <nav class="navbar navbar-dark bg-blue">
+        <div class="container-fluid">
+          <span class="navbar-text fw-bold">
+            RETURN TO HOME
+          </span>
+        </div>
+      </nav>
+    );
+}
