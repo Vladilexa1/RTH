3e0339027ced133503561eb8f6898bc6d50b5843
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2025-06-13 17:14:53 +0300

    feat: button block

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <nav class="footer bg-blue">
+        <div class="container-fluid">
+          <button class="btn btn-primary">SAVE</button>
+        </div>
+      </nav>
+    );
+}
