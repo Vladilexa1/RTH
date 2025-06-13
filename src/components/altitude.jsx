84acaf6429b63f79bd2135ec797dda5fd69f04c1
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2025-06-13 15:46:19 +0300

    feat: link the data of two inputs

--- src/components/altitude.jsx
+++ src/components/altitude.jsx
@@ -1,12 +1,30 @@
+import { createSignal } from "solid-js";
+
+
 export default function Altitude(props) {
     return (
-        <div class="altitude">
-            <input type="range" min="0" max="100" step="1" value="50" orient="vertical"/>  
-            <div>
-                <input type="number" class="number-altitude number-width"></input>
-                <label>m</label>
-            </div>   
-            <p class="title-alitude">{props.title}</p>
+      <div class="altitude">
+        <input
+          type="range"
+          min={props.min}
+          max={props.max}
+          step="1"
+          value={props.value}
+          orient="vertical"
+          onInput={(e) => props.setValue(+e.target.value)}
+        />
+        <div>
+          <input
+            type="number"
+            class="number-altitude number-width"
+            value={props.value}
+            min={props.min}
+            max={props.max}
+            onInput={(e) => props.setValue(+e.target.value)}
+          />
+          <label>m</label>
         </div>
+        <p class="title-alitude">{props.title}</p>
+      </div>
     );
-}
+  }
--- src/components/altitudeBlock.jsx
+++ src/components/altitudeBlock.jsx
@@ -1,14 +1,26 @@
+import { createSignal } from "solid-js";
 import Altitude from "./altitude";
 
-export default function AltitudeBlock(props) {
-    return (
-        <div class="altitude-block bgcol bord">
-            <Altitude
-            title="Min altitude RTH"
-          />
-          <Altitude
-            title="Max altitude RTH"
-          />
-        </div>
-    );
+export default function AltitudeBlock() {
+  const [minValue, setMinValue] = createSignal();
+  const [maxValue, setMaxValue] = createSignal();
+
+  return (
+    <div class="altitude-block bgcol bord">
+      <Altitude
+        title="Min altitude RTH"
+        value={minValue()}
+        setValue={setMinValue}
+        min={0}
+        max={maxValue()} // ограничиваем сверху
+      />
+      <Altitude
+        title="Max altitude RTH"
+        value={maxValue()}
+        setValue={setMaxValue}
+        min={minValue()} // ограничиваем снизу
+        max={100}
+      />
+    </div>
+  );
 }
