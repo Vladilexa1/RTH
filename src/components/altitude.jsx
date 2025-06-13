d4c7d0ba43bce4d7bbe8f17acca38829b58975ef
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2025-06-13 14:33:26 +0300

    feat: symbol metr

--- src/components/altitude.jsx
+++ src/components/altitude.jsx
@@ -2,7 +2,10 @@ export default function Altitude(props) {
     return (
         <div class="altitude">
             <input type="range" min="0" max="100" step="1" value="50" orient="vertical"/>  
-            <input type="number" class="number-altitude number-width"></input>
+            <div>
+                <input type="number" class="number-altitude number-width"></input>
+                <label>m</label>
+            </div>   
             <p class="title-alitude">{props.title}</p>
         </div>
     );
